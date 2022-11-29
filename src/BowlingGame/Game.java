2ff46da35319bd0a3a9812c33f99b8d57ba0356d
Felipe <luis.lgarcia@mercadolibre.com.co>
2022-11-28 21:59:27 -0500

    Test 5: all ones (Blue)

--- src/BowlingGame/Game.java
+++ src/BowlingGame/Game.java
@@ -2,13 +2,18 @@ package BowlingGame;
 
 public class Game {
 
-  private int score;
+  private int rolls[] = new int[21];
+  private int currentRoll = 0;
 
   public void roll(int pins) {
-    score += pins;
+    rolls[currentRoll++] = pins;
   }
 
   public int score() {
+    int score = 0;
+    for (int i = 0; i < rolls.length; i++) {
+      score += rolls[i];
+    }
     return score;
   }
 }
