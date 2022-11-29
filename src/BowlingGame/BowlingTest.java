44b2d349629b56ec5a3b308d71b6b9c62309d7c3
Felipe <luis.lgarcia@mercadolibre.com.co>
2022-11-28 20:03:47 -0500

    Test 3: gutter game (Red)

--- src/BowlingGame/BowlingTest.java
+++ src/BowlingGame/BowlingTest.java
@@ -1,13 +1,16 @@
 package BowlingGame;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class BowlingTest {
 
   private Game g;
 
-  @BeforeEach
+  @Before
   void setUp() {
     g = new Game();
   }
@@ -15,6 +18,14 @@ public class BowlingTest {
   @Test
   public void canRoll() throws Exception {
     g.roll(0);
+  }
+
+  @Test
+  public void gutterGame() throws Exception {
+    for(int i = 0; i < 20; i++){
+      g.roll(0);
+    }
+    assertEquals(0,g.score());
   }
 
 }
